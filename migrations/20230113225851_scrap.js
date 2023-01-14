48c8adfa12b7c0314ea6718b4c8ad805701a7aa0
TImoteoVieira <timoteo7charles777@gmail.com>
2023-01-14 20:23:50 -0300

    [add]: migracoes

---
+++ migrations/20230113225851_scrap.js
@@ -0,0 +1,17 @@
+const tableName = "scrapp";
+exports.up = async function(knex) {
+    await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
+    await knex.schema.createTable(tableName, function(table){
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.string('title'),
+    table.string('amount'),
+    table.text('description'),
+    table.string('data_rating'),
+    table.string('pull_right'),
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+})
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable(tableName);
+};
