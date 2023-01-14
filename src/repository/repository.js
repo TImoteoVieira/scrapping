b6c797a195cc5bb068621ce942da59eede016299
TImoteoVieira <timoteo7charles777@gmail.com>
2023-01-14 20:26:24 -0300

    [add]: camada de interacao com o banco de dados

---
+++ src/repository/repository.js
@@ -0,0 +1,30 @@
+const { connectionPostgreSQL } = require("../config/postgres");
+
+const tableName = 'scrapp';
+
+module.exports = class Repository {
+
+    async update(options){
+        try {
+            await connectionPostgreSQL(tableName).insert(options);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+
+    }
+    async clear(){
+        const result = await connectionPostgreSQL(tableName).del().returning('*');
+        return result;
+    }
+    async list(){
+        try {
+            const result = await connectionPostgreSQL().select().from(tableName).whereRaw('description ilike ?', '%lenovo%');
+            return result;
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+}
